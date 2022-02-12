f57a64f2ffbce6b0d418d964fb99d5c6d1f786c0
Joel Calado <joelcalado@gmail.com>
2022-02-12 22:48:50 +0000

    Update convert-tex-to-txt.py
    - defaults to  ../songs/pt as the input, making it work out of the box
    - moves paths to constants
    - add a --keep-chords flag, that, uh, keeps chords :)
    - creates output folder if it doesn't exist instead of doing nothing

--- scripts/convert-tex-to-txt.py
+++ scripts/convert-tex-to-txt.py
@@ -7,6 +7,14 @@
 import re
 import os
 import csv
+import argparse
+
+INPUT_FOLDER = '../songs/pt/'
+OUTPUT_FOLDER = './txt/'
+
+parser = argparse.ArgumentParser(description='Convert Psalterio TeX files to txt')
+parser.add_argument('--keep-chords', default=False, action='store_true')
+args = parser.parse_args()
 
 def get_title(s):
     title = re.findall("(?s)beginsong{(.*?)}", s, flags = re.MULTILINE)
@@ -27,17 +35,24 @@ def get_verse(s):
 
      
     verse_no_chords = re.sub(r'(?s)\\\[(.*?)]','', parsed_verse, flags= re.MULTILINE)
-    return verse_no_chords
+
+    if args.keep_chords == True:
+        return parsed_verse
+    else:
+        return verse_no_chords
         
 with open('outputlog.csv', 'w', newline='') as csvfile:
     fieldnames = ['title', 'status']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
+
+    if not os.path.isdir(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
     
-    for file in os.listdir("input"):
+    for file in os.listdir(INPUT_FOLDER):
         try:
             if file.endswith(".tex"):
-                with open ('input/'+ file, 'r', encoding="utf8") as input:
+                with open (INPUT_FOLDER + file, 'r', encoding="utf8") as input:
                     output = ''
                     latex_file_str = input.read()
                     title = get_title(latex_file_str)
@@ -48,7 +63,7 @@ with open('outputlog.csv', 'w', newline='') as csvfile:
                         verse = get_verse(section)
                         output += verse
                     final = output.replace('\\','')
-                    outputfile = open('output/'+ file[:-4] +'.txt', 'w')
+                    outputfile = open(OUTPUT_FOLDER + file[:-4] +'.txt', 'w')
                     outputfile.writelines(final)
                     outputfile.close()
                     writer.writerow({'title':title, 'status':'converted'})
